b381dbb54f3b4a9259af7c087757b9b689be0d07
HariniMansani <42859566+harinimansani@users.noreply.github.com>
2022-01-11 14:54:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+async function run() {
+  console.log('Hello, world!');
+}
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${3000}/`);
-});
+run();
